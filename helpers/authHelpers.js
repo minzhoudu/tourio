4e26b28145d7752fa9a1c3228229b48980c00eda
Pavle <pavle.jovanovic@deptagency.com>
2023-04-23 22:17:39 +0200

    Cookie security changes for railway production

--- helpers/authHelpers.js
+++ helpers/authHelpers.js
@@ -18,8 +18,8 @@ exports.sendJwtWithResponse = (user, statusCode, res) => {
     const cookieOptions = {
         expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000),
         httpOnly: true,
+        secure: req.secure || req.headers["x-forwarded-proto"] === "https",
     };
-    if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
 
     // Remove the passwords from the json output
     user.password = undefined;
