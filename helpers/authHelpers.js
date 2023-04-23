1472597753d7f3296f5ad42f97bf5d07745d6e0e
Pavle <pavle.jovanovic@deptagency.com>
2023-04-23 22:25:15 +0200

    secure cookie heroku removed

--- helpers/authHelpers.js
+++ helpers/authHelpers.js
@@ -18,7 +18,7 @@ exports.sendJwtWithResponse = (user, statusCode, res) => {
     const cookieOptions = {
         expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000),
         httpOnly: true,
-        secure: req.secure || req.headers["x-forwarded-proto"] === "https",
+        secure: req.secure,
     };
 
     // Remove the passwords from the json output
