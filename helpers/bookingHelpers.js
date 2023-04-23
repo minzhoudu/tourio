f031c4178166820e1663256b467dc0aa07395003
Pavle <pavle.jovanovic@deptagency.com>
2023-04-24 00:38:59 +0200

    object undefined fix

--- helpers/bookingHelpers.js
+++ helpers/bookingHelpers.js
@@ -2,9 +2,9 @@ const Booking = require("../models/bookingModel");
 const User = require("../models/userModel");
 
 exports.createBookingCheckout = async (session) => {
-    const tour = session.data.object.client_reference_id;
-    const user = (await User.findOne({ email: session.data.object.customer_email })).id;
-    const price = session.data.object.amount_total / 100;
+    const tour = session.client_reference_id;
+    const user = (await User.findOne({ email: session.customer_email })).id;
+    const price = session.amount_total / 100;
 
     await Booking.create({ tour, user, price });
 };
